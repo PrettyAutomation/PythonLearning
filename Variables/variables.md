cbedc30da5cd7174a3cd334702b76fc915f63a8c
pretty.sanwale <pretty.sanwale@servicenow.com>
2019-07-27 14:49:32 +0530

    first commit

---
+++ Variables/variables.md
@@ -0,0 +1,16 @@
+### Type of variables
+#### String
+E.g.
+```
+a="Hello world"
+```
+### int
+E.g.
+```
+i =10
+```
+### Float
+e.g.
+```
+a=2.5
+```
---
+++ Variables/variables.py
@@ -0,0 +1,15 @@
+# simple variable of different types
+stringvar= "Pretty"
+print stringvar
+a = 10
+print a
+
+b = 4.05
+print b
+
+c = 30.0000000009777 # memory dependent float precision
+print c  # it will print 30.0000000009
+
+
+d = 0x12c
+print d  # output = 300 (16^2*1 + 16^1*2 + 16^0*c (c hexvalue =12) = 300)
